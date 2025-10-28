76aafc9f8aafc59bae1b5bca06c7304db2a931d0
Droyd77 <araag18@gmail.com>
2025-10-28 15:18:46 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppAg13-DS-II - Hotel/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppAg13_DS_II___Hotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

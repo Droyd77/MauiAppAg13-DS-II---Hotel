e18761dba1f5baa204d075efc0e442fd025e8f3f
Droyd77 <araag18@gmail.com>
2025-10-29 17:47:59 -0300

    atividade ag13

--- MauiAppAg13-DS-II - Hotel/App.xaml.cs
+++ MauiAppAg13-DS-II - Hotel/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new views.ContratacaoHospedagem());
+            MainPage = new NavigationPage(new views.ContratacaoHospedagem ());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- MauiAppAg13-DS-II - Hotel/views/ContratacaoHospedagem.xaml.cs
+++ MauiAppAg13-DS-II - Hotel/views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,8 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+	private async void OnSobreClicked(object sender,EventArgs e)
+	{
+		await Navigation.PushAsync(new Sobre());
+	}
 }
